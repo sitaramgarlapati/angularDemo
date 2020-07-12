18ceb4908f4c29c458dfa5d8fa4f9e10558755d0
sitaramgarlapati <51022447+sitaramgarlapati@users.noreply.github.com>
2020-07-13 01:30:44 +0530

    added forms examples

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CompTemplatesComponent } from './features/comp-templates/comp-templates.component';
+import { UserSettingsFormComponent } from './features/user-settings-form/user-settings-form.component';
 
 const routes: Routes = [
-  {
-    path: '', component: CompTemplatesComponent,
-  },
-  { path: '**', redirectTo: '' }
+  { path: '', pathMatch: 'full', redirectTo: 'comp-templates' },
+  {  path: 'comp-templates', component: CompTemplatesComponent},
+  { path: 'user-settings', component: UserSettingsFormComponent},
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { MaterialModule } from './material/material.module';
 import { SidenavComponent } from './sidenav/sidenav.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 //Related to Components & Tempalates
 import { CompTemplatesComponent } from './features/comp-templates/comp-templates.component';
-
+import { UserSettingsFormComponent } from './features/user-settings-form/user-settings-form.component';
 
 @NgModule({
   declarations: [
@@ -19,11 +22,14 @@ import { CompTemplatesComponent } from './features/comp-templates/comp-templates
     SidenavComponent,
     //Content Tempatles
     CompTemplatesComponent,
+    UserSettingsFormComponent
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
     BrowserAnimationsModule,
     MaterialModule
   ],
